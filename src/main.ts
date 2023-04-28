a8f2d5275bd75ab6b6444b785f5c0aac646a512b
luongdao <luong.dao@pionero.io>
2023-04-28 11:39:52 +0700

    add dotenv safe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv-safe'
+
+dotenv.config()
+
 export function forEach<T = any>(arr: T[], callback: (item: T, arr: T[], index: number) => void) {
   for (let i = 0; i < arr.length; i++) {
     callback(arr[i], arr, i)
