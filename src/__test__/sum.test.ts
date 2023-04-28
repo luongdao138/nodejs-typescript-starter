642b61d5a614507a578ec0330ac47dff79fc1e14
luongdao <luong.dao@pionero.io>
2023-04-28 17:31:57 +0700

    add test to husky

--- src/__test__/sum.test.ts
+++ src/__test__/sum.test.ts
@@ -1,4 +1,5 @@
 import { sum } from '../sum'
+import { dulicateNumberArr } from '../utils/duplicate'
 
 describe('test sum function', () => {
   it('test sum', () => {
@@ -7,5 +8,14 @@ describe('test sum function', () => {
 
   it('test sum 2', () => {
     expect(sum(3, 6)).toBeGreaterThan(2)
+  })
+})
+
+describe('test duplicate function', () => {
+  it('test duplicate 1', () => {
+    expect(dulicateNumberArr([1, 2, 4])).toHaveLength(3)
+    expect(dulicateNumberArr([1, 2, 4])[0]).toBe(2)
+    expect(dulicateNumberArr([1, 2, 4])[1]).toBe(4)
+    expect(dulicateNumberArr([1, 2, 4])[2]).toBe(8)
   })
 })
