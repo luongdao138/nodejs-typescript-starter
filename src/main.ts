669d6fd1df1e035f63a0cb127bb0ca1b5172ba5b
luongdao <luong.dao@pionero.io>
2023-05-01 08:53:06 +0700

    setup server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,47 @@
+/* eslint-disable no-console */
 import dotenv from 'dotenv-safe'
+import express from 'express'
+
+import { appLoader } from '../nodejs-package-starter/dist'
+import { GracefulShutdownServer } from '../nodejs-package-starter/dist/utils'
 
 dotenv.config()
+;(async () => {
+  const start = async () => {
+    const app = express()
+    const directory = process.cwd()
 
-export function forEach<T = any>(arr: T[], callback: (item: T, arr: T[], index: number) => void) {
-  for (let i = 0; i < arr.length; i++) {
-    callback(arr[i], arr, i)
+    try {
+      await appLoader({ directory, expressApp: app })
+
+      const port = process.env.PORT ?? 9000
+
+      const server = GracefulShutdownServer.create(
+        app.listen(port, () => {
+          console.log(`Server listening on port ${port}`)
+        }),
+      )
+
+      // Handle graceful shutdown
+      const gracefulShutDown = () => {
+        server
+          .shutdown()
+          .then(() => {
+            console.info('Gracefully stopping the server.')
+            return process.exit(0)
+          })
+          .catch((e) => {
+            console.error('Error received when shutting down the server.', e)
+            return process.exit(1)
+          })
+      }
+      process.on('SIGTERM', gracefulShutDown)
+      process.on('SIGINT', gracefulShutDown)
+    } catch (error) {
+      console.error('Error starting server.', error)
+      process.exit(1)
+    }
   }
-}
+
+  await start()
+})()
