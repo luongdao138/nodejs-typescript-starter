aeb10fef82e27d7d5c4f69eec9441ef61c4964cc
luongdao <luong.dao@pionero.io>
2023-05-03 20:27:50 +0700

    add test custom loader

---
+++ src/loaders/test.ts
@@ -0,0 +1,11 @@
+import { Express } from 'express'
+
+import { AppContainer, Logger } from '../../nodejs-package-starter/dist'
+
+export default async function (container: AppContainer, app: Express) {
+  const logger = container.resolve<Logger>('logger')
+  app.use((_, __, next) => {
+    logger.info('This is logging from custom loader!')
+    next()
+  })
+}
